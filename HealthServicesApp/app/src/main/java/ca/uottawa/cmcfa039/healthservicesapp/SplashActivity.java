b3dd15c4c84df48c411018a5dae29fab04250f5a
Colin McFarlane <colinmcf0@gmail.com>
2019-10-20 19:37:41 -0400

    writing changes in splash

--- HealthServicesApp/app/src/main/java/ca/uottawa/cmcfa039/healthservicesapp/SplashActivity.java
+++ HealthServicesApp/app/src/main/java/ca/uottawa/cmcfa039/healthservicesapp/SplashActivity.java
@@ -82,7 +82,7 @@ public class SplashActivity extends AppCompatActivity {
                     String type = "Patient";
 
                     welcomeEditText.setText("Welcome " + firstName);
-                    signInEditText.setText("You're a: " + type);
+                    signInEditText.setText("You're logged-in as a: " + type);
                 }
 
             }
@@ -107,7 +107,7 @@ public class SplashActivity extends AppCompatActivity {
                     String type = "Employee";
 
                     welcomeEditText.setText("Welcome " + firstName);
-                    signInEditText.setText("You're an: " + type);
+                    signInEditText.setText("You're logged-in as a: " + type);
                 }
 
             }
@@ -130,7 +130,7 @@ public class SplashActivity extends AppCompatActivity {
                     String type = "Admin";
 
                     welcomeEditText.setText("Welcome " + firstName);
-                    signInEditText.setText("You're an: " + type);
+                    signInEditText.setText("You're logged-in as a: " + type);
                 }
             }
 
